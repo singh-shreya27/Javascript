09e3a651e3904c1d88a125ae0a87ce4033cef086
Shreya Singh <singh.s.shreya2004@gmail.com>
2024-12-23 18:34:43 +0530

    Update Classes and Objects.js

--- Classes and Objects.js
+++ Classes and Objects.js
@@ -163,3 +163,168 @@ class Parent{
 clas Child extends Parent{
 }
 //*If child & parent have same method child's method will be used[Method Overriding]
+
+class Parent{
+    hello(){
+        console.log("hello");
+    }
+}
+class Child extends Parent{}
+let obj=new Child();
+
+class Person{
+    constructor(){
+        this.species="homo sapiens";
+    }
+    eat(){
+        console.log("eat");
+    }
+    sleep(){
+        console.log("sleep");
+    }
+    work(){
+        console.log("do nothing.");
+    }
+}
+
+class Engineer extends Person{
+    work(){
+        console.log("Solve problems, build something.");
+    }   
+}
+class Doctor extends Person{
+    work(){
+        console.log("Treat patients.");
+    }   
+}
+
+let shreyaObj = new Engineer();
+
+//Super Keyword
+//The super keyword is used to call the constructor of its parent class to access the parent's properties and methods.
+
+super(args) //calls Parent's constructor
+super.parentMethod(args)
+
+lass Person{
+    constructor(){
+        console.log("enter parent constructor");
+
+        this.species="homo sapiens";
+    }
+    eat(){
+        console.log("eat");
+    }
+
+}    
+
+class Engineer extends Person{
+    constructor(branch){
+        console.log("enter child constructor");
+        super();   //to invoke parent class constructor.
+        this.branch = branch;
+        console.log("exit child constructor");
+
+    }
+    work(){
+        console.log("Solve problems, build something.");
+    }   
+}
+
+let engObj = new Engineer("chemical engg");
+
+class Person{
+    constructor(name){
+        this.species="homo sapiens";
+        this.name=name;
+    }
+    eat(){
+        console.log("eat");
+    }
+
+}    
+
+class Engineer extends Person{
+    constructor(name){
+        super(name);   //to invoke parent class constructor.
+
+    }
+    work(){
+              super.eat();
+
+        console.log("Solve problems, build something.");
+    }   
+}
+
+let engObj = new Engineer("shreya");
+
+//Q.1.You are creating a website for your college. Create a class User with 2 properties, name and email.It also has a method called viewData() that allows user to view website data.
+ 
+let DATA ="secret information";
+class User{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    viewData(){
+       console.log("data:",DATA);
+    }
+}
+
+let student1 = new User("shreya","abc@gmail.com");
+let student2 = new User("self","xyz@gmail.com");
+
+let teacher = new User("dean","dean@college.com");
+
+//Q.2. Create a new class called Admin which inherits from User.Add a new method caled editData to Admin that allows it to edit website data.
+let DATA ="secret information";
+class User{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    viewData(){
+       console.log("data:",DATA);
+    }
+}
+
+class Admin extends User{
+    constructor(name,email){
+        super(name,email);
+    }
+ editData(){
+    DATA = "some new value";
+ }
+}
+
+let student1 = new User("shreya","abc@gmail.com");
+let student2 = new User("self","xyz@gmail.com");
+
+let teacher = new User("dean","dean@college.com");
+
+let admin1 = new Admin("admin","admin@college.com");
+
+                      //Error Handling
+//try-catch blocks
+  try{
+     ...normal code
+  } catch(err){
+    //err is error object
+    ...handling error
+}
+
+let a=5;
+ let b=10;
+ console.log("a=",a);
+ console.log("b=",b);
+ console.log("a+b=",a+b);
+ console.log("a+b=",a+b);
+ try{
+ console.log("a+b=",a+c);  //error
+ }catch(err){
+   console.log(err);
+ }
+ console.log("a+b=",a+b);
+ console.log("a+b=",a+b);
+ console.log("a+b=",a+b);
+ console.log("a+b=",a+b);
